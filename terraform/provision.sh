5ba736bdc9ef52c63b52ca3a13890db2399e21e3
Andrew Liberty <andrew.c.liberty@gmail.com>
2017-09-20 17:18:45 -0700

    Update provision.sh
    Avoiding interactive dialog

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -4,7 +4,7 @@ echo "Provisioning boostrap machine ..."
 echo "Installing necessary packages ..."
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe"
 sudo apt-get update -y
-sudo apt-get upgrade -y
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
 sudo apt-get install -y build-essential git
 sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python
 sudo apt-get update -y
