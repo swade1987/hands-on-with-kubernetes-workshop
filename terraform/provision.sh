1dbfc9d8995a443b118a9a86f8787508965907ce
Andrew Liberty <aiberty@apprenda.com>
2017-10-10 14:24:59 -0400

    Adding support for DigitalOcean spaces

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -3,11 +3,14 @@ echo "Provisioning boostrap machine ..."
 
 echo "Installing necessary packages ..."
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe"
-sudo apt-get update -y
-DEBIAN_FRONTEND=noninteractive apt-get upgrade -yq
-sudo apt-get install -y build-essential git
-sudo apt-get install -qq python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python
-sudo apt-get update -y
+sudo apt-get -qq update
+DEBIAN_FRONTEND=noninteractive apt-get -qq upgrade
+sudo apt-get -qq install -y build-essential git
+sudo apt-get -qq install python2.7 && ln -s /usr/bin/python2.7 /usr/bin/python
+sudo apt-get -qq update
 
-echo "Clone the repo"
+echo "Clone the repo ..."
 git clone https://github.com/apprenda/hands-on-with-kubernetes-workshop.git
+
+echo "Downloading KET ..."
+make get-dependencies -C ~/hands-on-with-kubernetes-workshop/
